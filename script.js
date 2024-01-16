b88e9ba2cd93c63d8d36d5aed194a1d36fffa735
Mathabela Victor <mathabelavector@gmail.com>
2024-01-17 04:48:58 +1400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,135 @@
+function login() {
+ var loginButton = document.getElementById('loginForm').querySelector('button');
+ loginButton.innerHTML = '<span class="spinner-border spinner-border-sm " role="status" aria-hidden="true"></span> Loading...';
+ loginButton.disabled = true;
+
+ // Simulate a delay for the loading effect (you can replace this with your actual login logic)
+ setTimeout(function() {
+   // Reset the button after the delay
+   loginButton.innerHTML = '<i class="fas fa-sign-in-alt"></i> Login';
+   loginButton.disabled = false;
+ }, 2000); // Change the value as needed
+}
+
+function register() {
+  var loginButton = document.getElementById('loginForm').querySelector('button');
+  loginButton.innerHTML = '<span class="spinner-border spinner-border-sm " role="status" aria-hidden="true"></span> Loading...';
+  loginButton.disabled = true;
+ 
+  // Simulate a delay for the loading effect (you can replace this with your actual login logic)
+  setTimeout(function() {
+    // Reset the button after the delay
+    loginButton.innerHTML = '<i class="fas fa-check"  ></i> Create Account';
+    loginButton.disabled = false;
+  }, 2000); // Change the value as needed
+ }
+
+
+document.addEventListener("DOMContentLoaded", function () {
+ // Particle.js configuration
+ particlesJS("particles-js", {
+   particles: {
+     number: {
+       value: 100,
+       density: {
+         enable: true,
+         value_area: 800,
+       },
+     },
+     color: {
+       value: "#ffffff",
+     },
+     shape: {
+       type: "circle",
+       stroke: {
+         width: 0,
+         color: "#000000",
+       },
+       polygon: {
+         nb_sides: 5,
+       },
+     },
+     opacity: {
+       value: 0.5,
+       random: true,
+       anim: {
+         enable: false,
+         speed: 1,
+         opacity_min: 0.1,
+         sync: false,
+       },
+     },
+     size: {
+       value: 3,
+       random: true,
+       anim: {
+         enable: false,
+         speed: 40,
+         size_min: 0.1,
+         sync: false,
+       },
+     },
+     line_linked: {
+       enable: true,
+       distance: 150,
+       color: "#ffffff",
+       opacity: 0.4,
+       width: 1,
+     },
+     move: {
+       enable: true,
+       speed: 6,
+       direction: "none",
+       random: false,
+       straight: false,
+       out_mode: "out",
+       bounce: false,
+       attract: {
+         enable: false,
+         rotateX: 600,
+         rotateY: 1200,
+       },
+     },
+   },
+   interactivity: {
+     detect_on: "canvas",
+     events: {
+       onhover: {
+         enable: true,
+         mode: "repulse",
+       },
+       onclick: {
+         enable: true,
+         mode: "push",
+       },
+       resize: true,
+     },
+     modes: {
+       grab: {
+         distance: 400,
+         line_linked: {
+           opacity: 1,
+         },
+       },
+       bubble: {
+         distance: 400,
+         size: 40,
+         duration: 2,
+         opacity: 8,
+         speed: 3,
+       },
+       repulse: {
+         distance: 200,
+         duration: 0.4,
+       },
+       push: {
+         particles_nb: 4,
+       },
+       remove: {
+         particles_nb: 2,
+       },
+     },
+   },
+   retina_detect: true,
+ });
+});
